6072101c5f7b31b5549bb60a0f5cdfba0f83a1d9
slimane rechdi <slimane.rechdi@gmail.com>
2020-08-12 15:05:12 -0400

    refactoring

---
+++ {{cookiecutter.project_name}}/__main__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+"""
+Template SolidEdge with Ironpython.
+"""
+
+import sys
+import clr
+
+clr.AddReference("Interop.SolidEdge")
+clr.AddReference("System")
+clr.AddReference("System.Runtime.InteropServices")
+
+import System
+import System.Runtime.InteropServices as SRI
+from System import Console
+from System.IO import Directory
+from System.IO.Path import Combine
+
+
+def main():
+    try:
+        application = SRI.Marshal.GetActiveObject("SolidEdge.Application")
+        asm = application.ActiveDocument
+        print("part: %s\n" % asm.Name)
+        # asm.Type =>  plate :4 , assembly : 3, partdocument: 1
+        assert asm.Type == 3, "This macro only works on .asm"
+
+        # Start code here.
+
+
+    except AssertionError as err:
+        print(err.args)
+
+    except Exception as ex:
+        print(ex.args)
+
+    finally:
+        raw_input("\nPress any key to exit...")
+        sys.exit()
+
+
+def confirmation(func):
+    response = raw_input("""Make a .jt format file? (Press y/[Y] to proceed.)""")
+    if response.lower() not in ["y", "yes", "oui"]:
+        print("Process canceled")
+        sys.exit()
+    else:
+        func()
+
+
+if __name__ == "__main__":
+    confirmation(main)
--- {{cookiecutter.project_name}}/compile.bat
+++ {{cookiecutter.project_name}}/compile.bat
@@ -1,14 +1,11 @@
-:: app name: {{cookiecutter.project_name}}
+
 :: Set this file for compiling the executable of the macro.
-:: So it can be added to the user custom theme in solidedge. 
-ipyc.exe /main:./{{cookiecutter.project_name}}/__main__.py ^
-./{{cookiecutter.project_name}}/Interop.SolidEdge.dll ^
-./{{cookiecutter.project_name}}/api.py ./bullet/holes.py ^
-./{{cookiecutter.project_name}}/equivalences.py ^
-./{{cookiecutter.project_name}}/customs.py ^
+
+ipyc.exe /main:__main__.py ^
+Interop.SolidEdge.dll ^
 /embed ^
 /out:{{cookiecutter.project_name}}_macro_64x_0-0-0 ^
 /platform:x64 ^
 /standalone ^
 /target:exe ^
-/win32icon:logo.ico 
+/win32icon:logo.ico
